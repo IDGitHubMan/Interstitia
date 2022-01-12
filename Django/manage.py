6437513c6935428e2e62a841958193360ec72504
IDGitHubMan <40811090+IDGitHubMan@users.noreply.github.com>
2022-01-12 16:56:57 -0500

    Set up django again, because there was a bug before.

--- Django/manage.py
+++ Django/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InterstitiaStudios.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InterstitiaWebsite.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

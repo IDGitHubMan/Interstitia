f403ddcbf831ac151f738e0d539179d9ad127ea4
IDGitHubMan <40811090+IDGitHubMan@users.noreply.github.com>
2022-01-14 17:16:10 -0500

    Set up template rendering for about, collection, and home pages

--- Django/InterstitiaWebsite/views.py
+++ Django/InterstitiaWebsite/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+
+collect = {"Google Trends Maps":{"maker":"Isaiah Desrosiers","description":"A collection of maps that pull and visualize trending and top searches according to Google Trends.","tags":"mapbox dataviz webscraping javascript "},"Distribution":{"maker":"Isaiah Desrosiers","description":"A program that visualizes drawing random numbers with different means.","tags":"processing dataviz"}}
 def index(request):
-    return render(request,"https://raw.githubusercontent.com/IDGitHubMan/Interstitia/master/Django/InterstitiaWebsite/templates/index.html?token=GHSAT0AAAAAABQCAO5QYJT55PVIIHG4YX44YPANQWA#")
+    return render(request,"index.html")
+
+def about(request):
+    return render(request,"about.html")
+
+def collection(request):
+    collection = {"collect":collect}
+    return render(request,"collection.html",collection)

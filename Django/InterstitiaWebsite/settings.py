1f5e293c3b88554375911352aee5d81a59190889
IDGitHubMan <40811090+IDGitHubMan@users.noreply.github.com>
2022-04-13 10:57:08 -0400

    Add favicon

--- Django/InterstitiaWebsite/settings.py
+++ Django/InterstitiaWebsite/settings.py
@@ -115,6 +115,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+PROJECT_DIR = "/Users/idesrosiers/Documents/Interstitia/Django/InterstitiaWebsite"
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (

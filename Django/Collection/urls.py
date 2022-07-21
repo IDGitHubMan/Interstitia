ea8ec16fe23ee7e4371ed9699e40b4ff8a07b7e0
IDGitHubMan <40811090+IDGitHubMan@users.noreply.github.com>
2022-07-21 11:14:53 -0400

    Not sure what these are, but they exist now.

---
+++ Django/Collection/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from Collection import views
+import csv
+import glob
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
+
+g = glob.glob("/Users/idesrosiers/Documents/Projects/Interstitia/Django/InterstitiaWebsite/templates/ProjectPages/*.html")
+for file in g:
+    if (" " in file[99:len(file) - 5]):
+        s = file[99:len(file) - 5].replace(" ","_")
+    else:
+        s = file[99:len(file) - 5]
+    urlpatterns.append(path(''+s,getattr(views,s),name=s))

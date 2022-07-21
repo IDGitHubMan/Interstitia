0929ec7b51dcdc95d4764a9574283c8f14621de6
IDGitHubMan <40811090+IDGitHubMan@users.noreply.github.com>
2022-07-21 11:18:02 -0400

    Update urls.py

--- Django/InterstitiaWebsite/urls.py
+++ Django/InterstitiaWebsite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from dataclasses import replace
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 import glob
 
@@ -29,43 +29,13 @@ f.write("def index(request):\n")
 f.write("\treturn render(request,'index.html')\n")
 f.write("def about(request):\n")
 f.write("\treturn render(request,'about.html')\n")
-f.write("def collection(request):\n")
-f.write('\tresponse = requests.get("https://docs.google.com/spreadsheets/d/1LUZkRxjjDxCkUxpn3UHiIscy5dvs3BBfcPNF8C8FRns/export?format=csv")\n')
-f.write('\tf = open("/Users/idesrosiers/Documents/Projects/Interstitia/Django/projects.csv", "wb")\n')
-f.write('\tf.write(response.content)\n')
-f.write('\tf.close()\n')
-f.write('\twith open("/Users/idesrosiers/Documents/Projects/Interstitia/Django/projects.csv") as f:\n')
-f.write('\t\tcsv_reader = csv.reader(f,delimiter=",")\n')
-f.write('\t\tline_count = 0\n')
-f.write('\t\tfor row in csv_reader:\n')
-f.write('\t\t\tif line_count == 0:\n')
-f.write('\t\t\t\tline_count += 1\n')
-f.write('\t\t\telse:\n')
-f.write('\t\t\t\tcollect[row[0]] = {"maker":row[3],"description":row[1],"tags":row[7],"type":row[2],"contributors":row[4],"thumb":row[8],"formatted":row[9]}\n')
-f.write('\t\t\t\tfileString = "/Users/idesrosiers/Documents/Projects/Interstitia/Django/InterstitiaWebsite/templates/ProjectPages/" + row[0] + ".html"\n')
-f.write('\t\t\t\tif row[5] != "":\n')
-f.write('\t\t\t\t\tgetString = row[5][:len(row[5]) - 5] + "export?format=html?"\n')
-f.write('\t\t\t\t\tres = requests.get(getString)\n')
-f.write('\t\t\t\t\tif (not os.path.exists(fileString)):\n')
-f.write('\t\t\t\t\t\thtml = open(fileString,"wb")\n')
-f.write('\t\t\t\t\t\thtml.write(res.content)\n')
-f.write('\t\t\t\t\t\thtml.close()\n')
-f.write('\t\t\t\telse:\n')
-f.write('\t\t\t\t\thtml=open(fileString,"wb")\n')
-f.write('''\t\t\t\t\thtml.write('{% extends "base.html" %}'.encode() )\n''')
-f.write('\t\t\t\t\thtml.close()\n')
-f.write('\t\t\t\tdirectoryString = "/Users/idesrosiers/Documents/Projects/Interstitia/Django/InterstitiaWebsite/static/images/" + row[0]\n')
-f.write('\t\t\t\tif not os.path.exists(directoryString):\n')
-f.write('\t\t\t\t\tos.mkdir(directoryString)\n')
-f.write('\tcollection = {"collect":collect}\n')
-f.write('\treturn render(request,"collection.html",collection)')
 f.close()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name='index'),
     path('about',views.about,name='about'),
-    path('collection',views.collection,name='collection')
+    path('collection/', include('Collection.urls'))
 ]
 
 g = glob.glob("/Users/idesrosiers/Documents/Projects/Interstitia/Django/InterstitiaWebsite/templates/ProjectPages/*.html")
@@ -78,4 +48,3 @@ for file in g:
     f.write("\ndef " + s + "(request):")
     f.write('\n\treturn render(request,"' + file + '")')
     f.close()
-    urlpatterns.append(path('collection/'+s,getattr(views,s),name=s))

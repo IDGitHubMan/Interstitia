f771b7cf4c566efec64174fbffdb2cf7615adba5
IDGitHubMan <40811090+IDGitHubMan@users.noreply.github.com>
2022-08-15 13:04:17 -0400

    Add url for song collection

--- Django/apps/Cymatica/urls.py
+++ Django/apps/Cymatica/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from apps.Cymatica import views
 
 urlpatterns= [
-	path("/",views.index,name="Cymatica"),]
+	path("/",views.index,name="Cymatica"),
+	path("/song",views.songCollector,name="songCollector")
+]

f0cfb5b3a104a7ebba280e0720aa15d98d70beb7
IDGitHubMan <40811090+IDGitHubMan@users.noreply.github.com>
2022-08-15 13:05:08 -0400

    Begin formatting song fetcher

--- Django/apps/Cymatica/views.py
+++ Django/apps/Cymatica/views.py
@@ -2,6 +2,7 @@ import csv
 
 import requests
 from django.shortcuts import render
+from django.http import HttpResponse
 
 songList = {}
 def index(request):
@@ -19,3 +20,19 @@ def index(request):
 				songList[row[1]] = {"maker":row[2],"url":row[0]}
 	songs = {"songs":songList}
 	return render(request,"ProjectPages/Cymatica.html",songs)
+
+def songCollector(request):
+	response = requests.get("https://docs.google.com/spreadsheets/d/1VGrt5DrDCbnaq7gyg0v-PHiWIHPl3Q4_4cRL7ZFPVT8/export?format=csv")
+	f = open("songs.csv", "wb")
+	f.write(response.content)
+	f.close()
+	with open("songs.csv") as file:
+		reader = csv.reader(file,delimiter=",")
+		line_count = 0
+		for row in reader:
+			if line_count == 0:
+				line_count += 1
+			else:
+				songList[row[1]] = {"maker":row[2],"url":row[0]}
+	songs = {"songs":songList}
+	return HttpResponse("It worked!")

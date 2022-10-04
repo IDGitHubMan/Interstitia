2490a6df0bd720da5c344f55bf42cf5548cd2e3f
Isaiah Desrosiers <idguy2021@gmail.com>
2022-10-04 10:59:13 -0400

    Update urls.py to include only completed pages

--- Django/apps/Collection/urls.py
+++ Django/apps/Collection/urls.py
@@ -4,7 +4,8 @@ import glob
 import os;
 
 urlpatterns = [
-    path('/', views.index, name='colindex'),
+    path('', views.index, name='colindex'),
+    path('/Genera',include('apps.Genera.urls'),name='Genera')
 ]
 
 g = glob.glob("/Users/isaiahdesrosiers/Documents/Projects/Interstitia/Django/InterstitiaWebsite/templates/ProjectPages/*.html")
@@ -14,5 +15,5 @@ for file in g:
     else:
         s = file[99:len(file) - 5]
     if (s in os.listdir("/Users/isaiahdesrosiers/Documents/Projects/Interstitia/Django/apps")):
-        urlpatterns.append(path('/'+s,include('apps.'+s+'.urls'),name=s))
+        urlpatterns.append(path(s,include('apps.'+s+'.urls'),name=s))
         urlpatterns = [*set(urlpatterns)]

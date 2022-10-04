4cca3ee819f43000a88d88a43484f0b5194b375c
Isaiah Desrosiers <idguy2021@gmail.com>
2022-10-04 11:00:13 -0400

    Update urls.py

--- Django/apps/Cymatica/urls.py
+++ Django/apps/Cymatica/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from apps.Cymatica import views
 
 urlpatterns= [
-	path("/",views.index,name="Cymatica"),
+	path("",views.index,name="Cymatica"),
 	path("/song.mp3",views.songCollector,name="songCollector")
 ]

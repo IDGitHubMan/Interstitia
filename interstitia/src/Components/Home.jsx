3abcc749ab55cf71b0f8bef25ca176653d51c024
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-05-03 15:22:33 -0400

    Update Home.jsx

--- interstitia/src/Components/Home.jsx
+++ interstitia/src/Components/Home.jsx
@@ -1,44 +1,44 @@
-import { Link } from "react-router-dom";
 import Nav from "./Nav";
 import Preview from "./HomeAnim";
+import HomeSlides from "./HomeSlides";
+import { useEffect, useState } from "react";
 
 export const Home = () => {
+  const [slide, setSlide] = useState(0);
+  const slideContent = [
+    {
+      text: "Create and share beautiful generative art",
+      linkText: "Go to Genera",
+      linkDestination: "/genera",
+    },
+    {
+      text: "Explore our projects, whatever form they may take",
+      linkText: "View Our Collection",
+      linkDestination: "/collection",
+    },
+    {
+      text: "Learn about our stories and ideas",
+      linkText: "Meet our Team",
+      linkDestination: "/about",
+    },
+  ];
+  clearInterval();
+  useEffect(() => {
+    setInterval(() => {
+      setSlide(slide + 1);
+    }, 10000);
+  }, [slide]);
+  if (slide >= slideContent.length) {
+    setSlide(0);
+  }
   return (
     <div>
-      <div className="absolute top-0 left-0">
+      <div className="absolute top-0 left-0 w-full">
         <Preview />
       </div>
-      <div className="absolute top-0 left-0">
+      <div className="absolute top-0 left-0 w-full">
         <Nav />
-        <div class="bodySection flex justify-center align-middle items-center">
-          <div class="bodyText flex text-center align-middle justify-center rounded-md h-full w-1/2 flex-col items-center">
-            <p class="text-3xl">
-              Interstitia seeks to explore the spaces between disciplines with
-              explorative projects, whatever form that may take.
-            </p>
-            <Link to="/collection">
-              <div class="buttonLink w-max p-3 rounded-md text-black">
-                <p>View Our Collection</p>
-              </div>
-            </Link>
-          </div>
-          <div class="previewSlides">
-            <img id="previewImage" src="" width="" height="" alt="" />
-          </div>
-        </div>
-        <div class="bodySection flex justify-center align-middle items-center">
-          <div class="bodyText flex text-center align-middle justify-center rounded-md h-full w-1/2 flex-col items-center">
-            <p class="text-3xl">
-              We are a team of multipotential people from many backgrounds, each
-              with their own stories and ideas to tell and share.
-            </p>
-            <Link to="/about">
-              <div class="buttonLink w-max p-3 rounded-md text-black">
-                <p>Meet our Team</p>
-              </div>
-            </Link>
-          </div>
-        </div>
+        <HomeSlides slideNum={slide} slides={slideContent} />
       </div>
     </div>
   );

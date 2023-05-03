bc0ec5017e97443cae18b063686e052c0c297b42
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-05-03 15:21:57 -0400

    Create basic Node gen.

---
+++ interstitia/src/Components/Genera/NodeGen.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Sketch from "react-p5";
+import { Graph } from "./classes";
+let f;
+export const NodeGen = (props) => {
+  const setup = (p5, canvasParentRef) => {
+    p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
+    f = new Graph(p5);
+  };
+
+  const draw = (p5) => {
+    f.update();
+  };
+
+  const windowResized = (p5) => {
+    p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+  };
+
+  return <Sketch setup={setup} draw={draw} windowResized={windowResized} />;
+};
+
+export default NodeGen;

dfff2da80ee0581922a63304e47049ac50f8ab05
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-06-02 13:26:23 -0400

    Create StarGen.jsx

---
+++ interstitia/src/Components/Genera/StarGen.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Sketch from "react-p5";
+import { Star } from "../classes";
+let points = [];
+export const StarGen = (props) => {
+  let p5canvases;
+  const setup = (p5, canvasParentRef) => {
+    p5canvases = p5.selectAll(".react-p5");
+    p5.createCanvas(
+      p5canvases[0].size().width,
+      p5canvases[0].size().width
+    ).parent(canvasParentRef);
+    for (let i = 0; i < 1000; i++) {
+      points.push(new Star(p5));
+    }
+  };
+
+  const draw = (p5) => {
+    p5.background(0);
+    for (let s of points) {
+      s.twinkle();
+    }
+  };
+
+  const windowResized = (p5) => {
+    p5.resizeCanvas(p5canvases[0].size().width, p5canvases[0].size().width);
+  };
+
+  return <Sketch setup={setup} draw={draw} windowResized={windowResized} />;
+};
+export default StarGen;

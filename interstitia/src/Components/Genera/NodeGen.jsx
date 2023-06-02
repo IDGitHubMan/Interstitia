7ac948ea0f24e91d69cb3f748beabf9333c4a6d2
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-06-02 13:26:21 -0400

    Update NodeGen.jsx

--- interstitia/src/Components/Genera/NodeGen.jsx
+++ interstitia/src/Components/Genera/NodeGen.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Sketch from "react-p5";
-import { Graph } from "./classes";
+import { Graph } from "../classes";
 let f;
 export const NodeGen = (props) => {
+  let p5canvases;
   const setup = (p5, canvasParentRef) => {
+    p5canvases = p5.selectAll(".react-p5");
+    p5.print(p5canvases[0].size());
     p5.colorMode(p5.HSL, 360);
-    p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
+    p5.createCanvas(
+      p5canvases[0].size().width,
+      p5canvases[0].size().width
+    ).parent(canvasParentRef);
     f = new Graph(p5);
   };
 
@@ -14,7 +20,7 @@ export const NodeGen = (props) => {
   };
 
   const windowResized = (p5) => {
-    p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+    p5.resizeCanvas(p5canvases[0].size().width, p5canvases[0].size().width);
   };
 
   return <Sketch setup={setup} draw={draw} windowResized={windowResized} />;

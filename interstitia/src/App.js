b86cf5146d1e01f927fb4a5693f1a88e20e1980b
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-06-02 13:25:49 -0400

    Add Genera page.

--- interstitia/src/App.js
+++ interstitia/src/App.js
@@ -3,6 +3,7 @@ import { Route, BrowserRouter, Routes } from "react-router-dom";
 import { Home } from "./Components/Main/Home";
 import { useState } from "react";
 import NotFound from "./Components/Main/NotFound";
+import Genera from "./Components/Genera/Genera";
 
 function App() {
   const [signedIn, setSignedIn] = useState(false);
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/genera" element={<Genera />} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>

e99cc6a132400c6bea7f9afe895cbc6b904227db
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-06-02 13:26:28 -0400

    Update 404Stars.jsx

--- interstitia/src/Components/Main/404Stars.jsx
+++ interstitia/src/Components/Main/404Stars.jsx
@@ -3,7 +3,11 @@ import Sketch from "react-p5";
 import { Star } from "../classes";
 let points = [];
 export const Stars = (props) => {
+  let text;
+  let navbar;
   const setup = (p5, canvasParentRef) => {
+    text = p5.select(".text404");
+    navbar = p5.select("nav");
     p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
     for (let i = 0; i < 1000; i++) {
       points.push(new Star(p5));
@@ -15,12 +19,34 @@ export const Stars = (props) => {
     for (let s of points) {
       s.twinkle();
     }
+    text.position(
+      p5.width / 2 +
+        p5.cos(p5.millis() / 1000) * p5.atan(p5.millis() / 1000) * 15 -
+        text.size().width / 2,
+      p5.height / 2 +
+        p5.sin(p5.millis() / 1000) * p5.atan(p5.millis() / 1000) * 15 -
+        text.size().height / 2
+    );
+    let styleString = p5.join(
+      ["rotate(", p5.sin(p5.millis() / 1000) * 4, "deg)"],
+      ""
+    );
+    p5.print(styleString);
+    navbar.style("transform", styleString);
+    navbar.position(navbar.position().x, 80);
   };
 
   const windowResized = (p5) => {
     p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
   };
 
-  return <Sketch setup={setup} draw={draw} windowResized={windowResized} />;
+  return (
+    <Sketch
+      setup={setup}
+      draw={draw}
+      windowResized={windowResized}
+      className="fixed top-0 left-0"
+    />
+  );
 };
 export default Stars;

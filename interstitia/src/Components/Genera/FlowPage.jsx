dc1d93d6a2cdad832f6c558bcc7389c76c4ef7cb
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-06-04 19:59:42 -0400

    Rename files for clarity.

---
+++ interstitia/src/Components/Genera/FlowPage.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import NodeGen from "./NodeGen";
+import FlowGen from "./FlowGen";
+
+export const FlowPage = () => {
+  return (
+    <div>
+      <FlowGen></FlowGen>
+    </div>
+  );
+};
+export default FlowPage;

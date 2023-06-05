4ba756f861f9153232e49002acd0526eeae14924
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-06-05 14:58:28 -0400

    Add params to gen

--- interstitia/src/Components/Genera/CoreGen.jsx
+++ interstitia/src/Components/Genera/CoreGen.jsx
@@ -8,10 +8,42 @@ export const CoreGen = (props) => {
     p5canvases = p5.selectAll(".react-p5");
     p5.createCanvas(
       p5canvases[0].size().width,
-      p5canvases[0].size().width,
+      p5.min(p5canvases[0].size().width, p5.windowHeight),
       p5.WEBGL
     ).parent(canvasParentRef);
-    f = new Core(p5);
+    if (props.params == null) {
+      f = new Core(p5);
+    } else {
+      f = new Core(
+        p5,
+        [
+          parseInt(props.params.get("c1A")),
+          parseInt(props.params.get("c1B")),
+          parseInt(props.params.get("c1C")),
+        ],
+        [
+          parseInt(props.params.get("c2A")),
+          parseInt(props.params.get("c2B")),
+          parseInt(props.params.get("c2C")),
+        ],
+        parseInt(props.params.get("pt")),
+        parseInt(props.params.get("pc")),
+        props.params.get("rz") === "true",
+        props.params.get("ry") === "true",
+        props.params.get("rx") === "true",
+        parseInt(props.params.get("v")),
+        props.params.get("dp") === "true",
+        props.params.get("ds") === "true",
+        props.params.get("da") === "true",
+        props.params.get("fa") === "true",
+        [
+          parseInt(props.params.get("bgA")),
+          parseInt(props.params.get("bgB")),
+          parseInt(props.params.get("bgC")),
+          parseInt(props.params.get("bgD")),
+        ]
+      );
+    }
   };
 
   const draw = (p5) => {
@@ -19,7 +51,10 @@ export const CoreGen = (props) => {
   };
 
   const windowResized = (p5) => {
-    p5.resizeCanvas(p5canvases[0].size().width, p5canvases[0].size().width);
+    p5.resizeCanvas(
+      p5canvases[0].size().width,
+      p5.min(p5canvases[0].size().width, p5.windowHeight)
+    );
   };
 
   return <Sketch setup={setup} draw={draw} windowResized={windowResized} />;

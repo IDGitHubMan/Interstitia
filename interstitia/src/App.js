dbf6e477e16b8cb7e22853a4f185534e949efc3e
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-06-04 20:01:14 -0400

    Update App.js

--- interstitia/src/App.js
+++ interstitia/src/App.js
@@ -4,6 +4,8 @@ import { Home } from "./Components/Main/Home";
 import { useState } from "react";
 import NotFound from "./Components/Main/NotFound";
 import Genera from "./Components/Genera/Genera";
+import NodePage from "./Components/Genera/NodePage";
+import FlowPage from "./Components/Genera/FlowPage";
 
 function App() {
   const [signedIn, setSignedIn] = useState(false);
@@ -12,6 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/genera" element={<Genera />} />
+        <Route path="/genera/nodes" element={<NodePage />} />
+        <Route path="/genera/flow" element={<FlowPage />} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
